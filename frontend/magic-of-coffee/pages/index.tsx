16ba0eae188d2e06ae22808172c7faa4bb0a67f0
WiktoriaKrzyzanska <97694545+WiktoriaKrzyzanska@users.noreply.github.com>
2024-05-08 22:00:42 +0200

    added video and changed hoq the main page looks

--- frontend/magic-of-coffee/pages/index.tsx
+++ frontend/magic-of-coffee/pages/index.tsx
@@ -8,16 +8,18 @@ import CookiesConsent from '@/components/CookiesConsent';
 import Newsletter from '@/components/Newsletter';
 import Chatbot from '@/components/Chatbot';
 import Hero from '@/components/Hero';
+import Movie from '@/components/Movie';
 
 const Page: React.FC = () => (
     <Layout>
         <Header />
-        <MainBanner />
+        <Movie/>
         <MyPage/>
         <Hero 
         title="Discover the best coffee"
         description="Magic Coffee is a coffee shop that provides you with quality coffee that helps boost your productivity and helps build your mood. Having a cup of coffee is good, but having a cup of real coffee is greater. There is no doubt that you will enjoy this coffee more than others you have ever tasted."
       />
+     <MainBanner />
       <Hero 
         title="Get a chance to have the best morning"
         description="We are giving you are one time opportunity to
