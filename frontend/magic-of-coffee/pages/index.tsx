9a3bbe6cba6eef42ede0577d85df541bc901d03b
WiktoriaKrzyzanska <97694545+WiktoriaKrzyzanska@users.noreply.github.com>
2024-05-08 21:08:33 +0200

    added hero component

--- frontend/magic-of-coffee/pages/index.tsx
+++ frontend/magic-of-coffee/pages/index.tsx
@@ -7,12 +7,22 @@ import MyPage from '@/pages/myMap';
 import CookiesConsent from '@/components/CookiesConsent';
 import Newsletter from '@/components/Newsletter';
 import Chatbot from '@/components/Chatbot';
+import Hero from '@/components/Hero';
 
 const Page: React.FC = () => (
     <Layout>
         <Header />
         <MainBanner />
         <MyPage/>
+        <Hero 
+        title="Discover the best coffee"
+        description="Magic Coffee is a coffee shop that provides you with quality coffee that helps boost your productivity and helps build your mood. Having a cup of coffee is good, but having a cup of real coffee is greater. There is no doubt that you will enjoy this coffee more than others you have ever tasted."
+      />
+      <Hero 
+        title="Get a chance to have the best morning"
+        description="We are giving you are one time opportunity to
+        experience a better life with coffee."
+      />
         <Chatbot/>
         <Newsletter/>
         <Footer/>
