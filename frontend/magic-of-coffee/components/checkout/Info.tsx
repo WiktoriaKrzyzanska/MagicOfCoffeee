cf6706baadaf40683d45d8d42dd9fc3b0516d24a
WiktoriaKrzyzanska <97694545+WiktoriaKrzyzanska@users.noreply.github.com>
2024-05-09 12:59:19 +0200

    added checkout

---
+++ frontend/magic-of-coffee/components/checkout/Info.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
+
+const products = [
+  {
+    name: 'Coffee from Madagascar',
+    desc: '250g',
+    price: '$15.00',
+  },
+  {
+    name: 'Coffee from Krakov',
+    desc: '500g',
+    price: '$25.0',
+  },
+  {
+    name: 'Coffee from Argentina',
+    desc: '1kg',
+    price: '$69.99',
+  },
+  {
+    name: 'Chocolate form Ghana',
+    desc: '100g',
+    price: 'Gift',
+  },
+];
+
+interface InfoProps {
+  totalPrice: string;
+}
+
+export default function Info({ totalPrice }: InfoProps) {
+  return (
+    <React.Fragment>
+      <Typography variant="subtitle2" color="text.secondary">
+        Total
+      </Typography>
+      <Typography variant="h4" gutterBottom>
+        {totalPrice}
+      </Typography>
+      <List disablePadding>
+        {products.map((product) => (
+          <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
+            <ListItemText
+              sx={{ mr: 2 }}
+              primary={product.name}
+              secondary={product.desc}
+            />
+            <Typography variant="body1" fontWeight="medium">
+              {product.price}
+            </Typography>
+          </ListItem>
+        ))}
+      </List>
+    </React.Fragment>
+  );
+}
---
+++ frontend/magic-of-coffee/pages/checkout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Checkout from '@/components/checkout/Checkout'
+
+
+const CheckoutPage: React.FC = () => (
+            <Checkout/>
+);
+
+export default CheckoutPage;
